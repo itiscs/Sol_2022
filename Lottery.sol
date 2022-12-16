2f5b5dcffe8d84bfd1a0e4a09c202ee31256b823
itiscs <moudroff@gmail.com>
2022-12-16 15:09:17 +0300

    Lottery

---
+++ Lottery.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract Lottery{
+    //dynamic array with players payable addresses
+    //in solidity version newer than 0.5.0 an address must be payable to be able to receive funds (using the transfer function)
+    address payable[] public players; 
+    
+    address public manager; //contract manager
+    
+    //contract constructor, runs once at contract deployment
+    constructor() {
+        //the manager is the account address that deploys the contract
+        manager = msg.sender; 
+    }
+    
+    //this fallback payable function will be automatically called when somebody
+    //sends ether to our contract address
+    //since solidity 0.5.0 it's mandatory to be external
+    function recieve () payable external{
+        require(msg.value >= 0.001 ether);
+        players.push(payable(msg.sender)); //add the address of the account that sends
+                                  //ether to players array
+    }
+
+    function getPlayersCount() public view returns(uint){
+        return players.length;
+    }
+
+    function getManager() public view returns(address){
+        return manager;
+    }
+
+    function getPlayer(uint i) public view returns(address){
+        require(i<players.length);
+        return players[i];
+    }
+ 
+    function depositEther() external payable {
+         require(msg.value >= 0.001 ether);  // will work because you can receive
+         players.push(payable(msg.sender));
+
+    }
+
+
+    function get_balance() public view returns(uint){
+        require(msg.sender == manager);
+        return address(this).balance; //return contract balance
+    }
+    
+    //returns a very big pseodo-random integer no.
+    function random() public view returns(uint256){
+        
+       //since solidity 0.5.0  keccak256() function accepts only a single bytes argument
+       //we use the abi.encodePacked() function to get the bytes argument from 3 values
+       return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    }
+    
+    function selectWinner() public {
+        require(msg.sender == manager);
+        require(get_balance() > 0);
+        
+
+        uint r = random();
+        
+        address payable winner;
+        
+        //a random index
+        uint index = r % players.length;
+        winner = players[index];
+        
+        //transfer contract balance to the winner address
+        winner.transfer(address(this).balance);
+        
+        players = new address payable[](0); //resetting the players dynamic array
+    }
+}
